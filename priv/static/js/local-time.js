191d1fcb7196a9896b184659bd32815d7cd412c5
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-03-07 17:19:44 -0500

    Restore vichan local time script

--- priv/static/js/local-time.js
+++ priv/static/js/local-time.js
@@ -17,45 +17,29 @@ $(document).ready(function(){
 	'use strict';
 
 	var iso8601 = function(s) {
-		var parts = s.split('T');
-		if (parts.length === 2) {
-			var timeParts = parts[1].split(':');
-			if (timeParts.length === 3) {
-				var seconds = timeParts[2];
-				if (seconds.length > 2) {
-					seconds = seconds.substr(0, 2) + '.' + seconds.substr(2);
-				}
-				// Ensure seconds part is valid
-				if (parseFloat(seconds) > 59) {
-					seconds = '59';
-				}
-				timeParts[2] = seconds;
-			}
-			parts[1] = timeParts.join(':');
-		}
-		s = parts.join('T');
-
-		if (!s.endsWith('Z')) {
-			s += 'Z';
-		}
+		s = s.replace(/\.\d\d\d+/,""); // remove milliseconds
+		s = s.replace(/-/,"/").replace(/-/,"/");
+		s = s.replace(/T/," ").replace(/Z/," UTC");
+		s = s.replace(/([\+\-]\d\d)\:?(\d\d)/," $1$2"); // -04:00 -> -0400
 		return new Date(s);
 	};
-
 	var zeropad = function(num, count) {
 		return [Math.pow(10, count - num.toString().length), num].join('').substr(1);
 	};
 
 	var dateformat = (typeof strftime === 'undefined') ? function(t) {
 		return zeropad(t.getMonth() + 1, 2) + "/" + zeropad(t.getDate(), 2) + "/" + t.getFullYear().toString().substring(2) +
-				" (" + ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][t.getDay()] + ") " +
+				" (" + [_("Sun"), _("Mon"), _("Tue"), _("Wed"), _("Thu"), _("Fri"), _("Sat"), _("Sun")][t.getDay()]  + ") " +
 				// time
 				zeropad(t.getHours(), 2) + ":" + zeropad(t.getMinutes(), 2) + ":" + zeropad(t.getSeconds(), 2);
+
 	} : function(t) {
 		// post_date is defined in templates/main.js
 		return strftime(window.post_date, t, datelocale);
 	};
 
 	function timeDifference(current, previous) {
+
 		var msPerMinute = 60 * 1000;
 		var msPerHour = msPerMinute * 60;
 		var msPerDay = msPerHour * 24;
@@ -67,65 +51,37 @@ $(document).ready(function(){
 		if (elapsed < msPerMinute) {
 			return 'Just now';
 		} else if (elapsed < msPerHour) {
-			return Math.round(elapsed / msPerMinute) + (Math.round(elapsed / msPerMinute) <= 1 ? ' minute ago' : ' minutes ago');
-		} else if (elapsed < msPerDay) {
-			return Math.round(elapsed / msPerHour) + (Math.round(elapsed / msPerHour) <= 1 ? ' hour ago' : ' hours ago');
+			return Math.round(elapsed/msPerMinute) + (Math.round(elapsed/msPerMinute)<=1 ? ' minute ago':' minutes ago');
+		} else if (elapsed < msPerDay ) {
+			return Math.round(elapsed/msPerHour ) + (Math.round(elapsed/msPerHour)<=1 ? ' hour ago':' hours ago');
 		} else if (elapsed < msPerMonth) {
-			return Math.round(elapsed / msPerDay) + (Math.round(elapsed / msPerDay) <= 1 ? ' day ago' : ' days ago');
+			return Math.round(elapsed/msPerDay) + (Math.round(elapsed/msPerDay)<=1 ? ' day ago':' days ago');
 		} else if (elapsed < msPerYear) {
-			return Math.round(elapsed / msPerMonth) + (Math.round(elapsed / msPerMonth) <= 1 ? ' month ago' : ' months ago');
+			return Math.round(elapsed/msPerMonth) + (Math.round(elapsed/msPerMonth)<=1 ? ' month ago':' months ago');
 		} else {
-			return Math.round(elapsed / msPerYear) + (Math.round(elapsed / msPerYear) <= 1 ? ' year ago' : ' years ago');
+			return Math.round(elapsed/msPerYear ) + (Math.round(elapsed/msPerYear)<=1 ? ' year ago':' years ago');
 		}
 	}
 
-	var do_localtime = function(elem) {
+	var do_localtime = function(elem) {	
 		var times = elem.getElementsByTagName('time');
 		var currentTime = Date.now();
 
-		for (var i = 0; i < times.length; i++) {
+		for(var i = 0; i < times.length; i++) {
 			var t = times[i].getAttribute('datetime');
-			var postTime = iso8601(t);
+			var postTime = new Date(t);
 
 			times[i].setAttribute('data-local', 'true');
 
 			if (localStorage.show_relative_time === 'false') {
-				times[i].innerHTML = dateformat(postTime);
+				times[i].innerHTML = dateformat(iso8601(t));
 				times[i].setAttribute('title', timeDifference(currentTime, postTime.getTime()));
 			} else {
-				times[i].innerHTML = timeDifference(currentTime, postTime.getTime());
-				times[i].setAttribute('title', dateformat(postTime));
+				times[i].innerHTML = dateformat(iso8601(t));
+				times[i].setAttribute('title', timeDifference(currentTime, postTime.getTime()));
 			}
+		
 		}
 	};
-
-	if (window.Options && Options.get_tab('general') && window.jQuery) {
-		var interval_id;
-		Options.extend_tab('general', '<label id="show-relative-time"><input type="checkbox">' + _('Show relative time') + '</label>');
-
-		$('#show-relative-time>input').on('change', function() {
-			if (localStorage.show_relative_time !== 'false') {
-				localStorage.show_relative_time = 'false';
-				clearInterval(interval_id);
-			} else {
-				localStorage.show_relative_time = 'true';
-				interval_id = setInterval(do_localtime, 30000, document);
-			}
-			// no need to refresh page
-			do_localtime(document);
-		});
-
-		if (localStorage.show_relative_time !== 'false') {
-			$('#show-relative-time>input').attr('checked', 'checked');
-			interval_id = setInterval(do_localtime, 30000, document);
-		}
-
-		// allow to work with auto-reload.js, etc.
-		$(document).on('new_post', function(e, post) {
-			do_localtime(post);
-		});
-	}
-
 	do_localtime(document);
 });
-
