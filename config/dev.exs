151500bd71e1453041e072d1b4aa669d32083a98
username <username@example.com>
2026-03-07 09:11:46 -0500

    Expose dev server on port 4001

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :eirinchan, build_output_root: Path.expand("../tmp/dev_build", __DIR__)
 config :eirinchan, EirinchanWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4001],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
