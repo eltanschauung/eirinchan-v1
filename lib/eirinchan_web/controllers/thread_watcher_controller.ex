db0e117abab2efd744f5e6a3ec64ea336cc101c5
username <username@example.com>
2026-03-11 13:18:56 -0400

    Sync thread watcher seen state

--- lib/eirinchan_web/controllers/thread_watcher_controller.ex
+++ lib/eirinchan_web/controllers/thread_watcher_controller.ex
@@ -36,6 +36,28 @@ defmodule EirinchanWeb.ThreadWatcherController do
     end
   end
 
+  def update(conn, %{"board" => board_uri, "thread_id" => thread_id, "last_seen_post_id" => last_seen_post_id}) do
+    with {:ok, board} <- fetch_board(board_uri),
+         {:ok, thread} <- Posts.fetch_thread(board, thread_id),
+         {parsed_last_seen_post_id, ""} <- Integer.parse(to_string(last_seen_post_id)),
+         true <- parsed_last_seen_post_id >= thread.id,
+         {:ok, _watch} <-
+           ThreadWatcher.mark_seen(
+             conn.assigns.browser_token,
+             board.uri,
+             thread.id,
+             parsed_last_seen_post_id
+           ) do
+      json(conn, %{ok: true, thread_id: thread.id, last_seen_post_id: parsed_last_seen_post_id})
+    else
+      {:error, :not_found} -> send_resp(conn, :not_found, "")
+      {:error, :thread_not_found} -> send_resp(conn, :not_found, "")
+      false -> send_resp(conn, :unprocessable_entity, "")
+      :error -> send_resp(conn, :unprocessable_entity, "")
+      _ -> send_resp(conn, :unprocessable_entity, "")
+    end
+  end
+
   defp fetch_board(uri) do
     case Boards.get_board_by_uri(uri) do
       nil -> {:error, :not_found}
