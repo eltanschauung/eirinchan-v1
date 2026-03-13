7a9885de968c3648986be698cf7352e7b441910a
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-03-13 01:11:26 -0400

    Unify public shell boardlist and nav arrows

--- priv/static/js/mobile-style.js
+++ priv/static/js/mobile-style.js
@@ -11,7 +11,13 @@
  *   $config['additional_javascript'][] = 'js/mobile-style.js';
  */
 
-if(navigator.userAgent.match(/iPhone|iPod|iPad|Android|Opera Mini|Blackberry|PlayBook|Windows Phone|Tablet PC|Windows CE|IEMobile/i)) {
+if ($('html').hasClass('mobile-style')) {
+	device_type = "mobile";
+}
+else if ($('html').hasClass('desktop-style')) {
+	device_type = "desktop";
+}
+else if(navigator.userAgent.match(/iPhone|iPod|iPad|Android|Opera Mini|Blackberry|PlayBook|Windows Phone|Tablet PC|Windows CE|IEMobile/i)) {
 	$('html').addClass("mobile-style");
 	device_type = "mobile";
 }
