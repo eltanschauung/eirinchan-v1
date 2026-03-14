495c3aceaf7d4091c0f37ba7cba620340d06f338
eltanschauung <xxlibtardeaterxx@gmail.com>
2026-03-14 15:29:43 -0400

    Fix board stylesheet selection persistence

--- lib/eirinchan_web/plugs/fetch_theme.ex
+++ lib/eirinchan_web/plugs/fetch_theme.ex
@@ -55,7 +55,13 @@ defmodule EirinchanWeb.Plugs.FetchTheme do
   end
 
   defp decode_board_themes_cookie(value) when is_binary(value) do
-    case Jason.decode(value) do
+    decoded_value =
+      case URI.decode(value) do
+        ^value -> value
+        decoded -> decoded
+      end
+
+    case Jason.decode(decoded_value) do
       {:ok, %{} = decoded} -> decoded
       _ -> %{}
     end
