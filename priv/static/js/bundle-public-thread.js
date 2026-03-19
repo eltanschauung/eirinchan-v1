bca844107fe5f9bdb0acd3352cab6df7eb529e02
username <username@example.com>
2026-03-18 21:08:47 -0400

    Restore show-own-posts in public bundles

--- priv/static/js/bundle-public-thread.js
+++ priv/static/js/bundle-public-thread.js
@@ -3908,6 +3908,117 @@ onReady(function() {
 });
 /* End js/post-hover.js */
 
+/* Begin js/show-own-posts.js */
+/*
+ * show-own-posts.js
+ * https://github.com/savetheinternet/Tinyboard/blob/master/js/show-op.js
+ *
+ * Adds "(You)" to a name field when the post is yours. Update references as well.
+ *
+ * Released under the MIT license
+ * Copyright (c) 2014 Marcin Łabanowski <marcin@6irc.net>
+ *
+ * Usage:
+ *   $config['additional_javascript'][] = 'js/jquery.min.js';
+ *   $config['additional_javascript'][] = 'js/ajax.js';
+ *   $config['additional_javascript'][] = 'js/show-own-posts.js';
+ *
+ */
+
+
++function(){
+
+
+var update_own = function() {
+  if ($(this).is('.you')) return;
+
+  var thread = $(this).parents('[id^="thread_"]').first();
+  if (!thread.length) {
+    thread = $(this);
+  }
+
+  var board = thread.attr('data-board');
+  var posts = JSON.parse(localStorage.own_posts || '{}');
+
+  var id = $(this).attr('id').split('_')[1];
+
+  if (posts[board] && posts[board].indexOf(id) !== -1) { // Own post!
+    $(this).addClass('you');
+    $(this).find('span.name').first().append(' <span class="own_post">'+_('(You)')+'</span>');
+  }
+
+  // Update references
+  $(this).find('div.body:first a:not([rel="nofollow"])').each(function() {
+    var postID;
+
+    if(postID = $(this).text().match(/^>>(\d+)$/))
+      postID = postID[1];
+    else
+      return;
+
+    if (posts[board] && posts[board].indexOf(postID) !== -1) {
+      $(this).after(' <small>'+_('(You)')+'</small>');
+    }
+  });
+};
+
+var update_all = function() {
+  $('div[id^="thread_"], div.post.reply').each(update_own);
+};
+
+var board = null;
+
+$(function() {
+  board = $('input[name="board"]').first().val();
+
+  update_all();
+});
+
+$(document).on('ajax_after_post', function(e, r) {
+  var posts = JSON.parse(localStorage.own_posts || '{}');
+  posts[board] = posts[board] || [];
+  posts[board].push(r.id);
+  localStorage.own_posts = JSON.stringify(posts);
+});
+
+$(document).on('new_post', function(e,post) {
+  var $post = $(post);
+  if ($post.is('div.post.reply')) { // it's a reply
+    $post.each(update_own);
+  }
+  else {
+    $post.each(update_own); // first OP
+    $post.find('div.post.reply').each(update_own); // then replies
+  }
+});
+
+
+
+}();
+/* End js/show-own-posts.js */
+
+/* Begin js/show-own-posts-options.js */
+if (active_page === 'thread' || active_page === 'index' || active_page === 'catalog' || active_page === 'ukko') {
+  document.addEventListener('DOMContentLoaded', function () {
+    if (!(window.Options && Options.get_tab('general'))) return;
+
+    Options.extend_tab(
+      'general',
+      '<label id="show-yous"><input type="checkbox">' + _('Show (You)s') + '</label>'
+    );
+
+    var enabled = document.cookie.indexOf('show_yous=false') === -1;
+    $('#show-yous>input').prop('checked', enabled);
+
+    $('#show-yous>input').on('click', function () {
+      var value = $('#show-yous>input').is(':checked') ? 'true' : 'false';
+      document.cookie = 'show_yous=' + value + '; path=/; max-age=31536000; samesite=lax';
+      location.reload();
+    });
+  });
+}
+/* End js/show-own-posts-options.js */
+
 /* Begin js/fix-report-delete-submit.js */
 /*
  * fix-report-delete-submit.js
