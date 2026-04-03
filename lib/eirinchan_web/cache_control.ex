9e1ade5e8ce5944f5a353c2dc7700e27e1273426
username <username@example.com>
2026-04-03 00:25:53 -0400

    Improve CSS transitions with new caching system for assets

--- lib/eirinchan_web/cache_control.ex
+++ lib/eirinchan_web/cache_control.ex
@@ -11,19 +11,25 @@ defmodule EirinchanWeb.CacheControl do
   end
 
   def cache_control_for_path(path) when is_binary(path) do
-    case path |> Path.extname() |> String.downcase() do
-      ".gif" -> public(@one_month)
-      ".png" -> public(@one_month)
-      ".jpg" -> public(@one_month)
-      ".jpeg" -> public(@one_month)
-      ".webp" -> public(@one_month)
-      ".css" -> public(@one_minute)
-      ".js" -> public(@one_minute)
-      ".svg" -> immutable(@one_year)
-      ".ico" -> immutable(@one_year)
-      ".txt" -> public(@ten_minutes)
-      ".zip" -> public(@ten_minutes)
-      _ -> public(@ten_minutes)
+    cond do
+      String.starts_with?(path, "/stylesheets/assets/") ->
+        public(@one_minute)
+
+      true ->
+        case path |> Path.extname() |> String.downcase() do
+          ".gif" -> public(@one_month)
+          ".png" -> public(@one_month)
+          ".jpg" -> public(@one_month)
+          ".jpeg" -> public(@one_month)
+          ".webp" -> public(@one_month)
+          ".css" -> public(@one_minute)
+          ".js" -> public(@one_minute)
+          ".svg" -> immutable(@one_year)
+          ".ico" -> immutable(@one_year)
+          ".txt" -> public(@ten_minutes)
+          ".zip" -> public(@ten_minutes)
+          _ -> public(@ten_minutes)
+        end
     end
   end
 
